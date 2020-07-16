66f348b74c94a70a87d0e46f7dc16c7495993cae
Sathit Seethaphon <dixonsatit@gmail.com>
2020-07-16 10:51:20 +0700

    update version

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Build
+
+> brew install xctool
--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 
 electron-packager . EdcConnect --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds
-Packaging app for platform darwin x64 using electron v6.0.9
-Wrote new app to release-builds/EdcConnect-darwin-x64
 
 electron-packager . EdcConnect --overwrite --asar=true --platform=win32 --arch=ia32  --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="EDC Connect"
 
